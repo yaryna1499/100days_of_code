7cb56873a74fbd834fc24737736d3f92f561e4b2
Yaryna Ostapchuk <yyyzzz9914@gmail.com>
2022-06-11 13:14:44 +0300

    pong game\other

---
+++ pong.py
@@ -0,0 +1,39 @@
+from turtle import Screen, Turtle
+from paddle_module import Paddle
+from ball_module import Ball
+import time
+
+screen = Screen()  # creating screen object from the Screen class
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+ball = Ball()
+
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # Detect collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340:
+        ball.bounce_x()
+
+screen.exitonclick()
